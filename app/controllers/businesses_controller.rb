247ed929fbc38689125a8de5e46195bdedb0ee60
Chinh Le <chinh.t.le@gmail.com>
2014-06-14 18:39:34 -0700

    set initial styling of pie chart to pastel, and some formatting on the business page

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,5 +1,6 @@
 class BusinessesController < ApplicationController
     def index
+        #https://developers.google.com/chart/interactive/docs/gallery/piechart?csw=1
 
         data_table = GoogleVisualr::DataTable.new
         data_table.new_column('string', 'Personality')
@@ -16,9 +17,21 @@ class BusinessesController < ApplicationController
         data_table.set_cell(4, 0, 'Type 5'    )
         data_table.set_cell(4, 1, 7  )
 
-        opts   = { :width => 800, :height => 480,
+        # Assigning more colors than the actual number of elements is OK,
+        # but if there are more elements than colors then the scheme will be
+        # off! Make sure there are enough pastel colors! We will assume
+        # there will not be more elements, ever, than the actual number
+        # of pastel colors.
+        slice_pastel_colors = [{color: '#DEA5A4'}, {color: '#77DD77'},
+                               {color: '#AEC6CF'}, {color: '#B39EB5'},
+                               {color: '#CB99C9'}, {color: '#779ECB'},
+                               {color: '#836953'}, {color: '#FF6961'},
+                               {color: '#B39EB5'}, {color: '#FDFD96'}]
+
+        opts   = { :width => 800, :height => 400,
                    # :title => 'My Daily Activities',
-                   :pieHole => 0.5}
+                   :pieHole => 0.5, :legend => {position: 'bottom', maxLines: 3},
+                   :slices => slice_pastel_colors}
 
         @chart = GoogleVisualr::Interactive::PieChart.new(data_table, opts)
 
