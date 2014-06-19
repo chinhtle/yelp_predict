ac7afa2b5badfa32e0d271f4ce772de595314d01
Chinh Le <chinh.t.le@gmail.com>
2014-06-18 20:19:09 -0700

    Some changes on header and bootstrap.

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module YelpPredict
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Some compatibility recommendation for bootstrap.
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
