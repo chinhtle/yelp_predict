8e83b54334a34bd274aa77fab48c06ed84a0d867
Chinh Le <chinh.t.le@gmail.com>
2014-07-22 01:49:47 -0700

    Fix leading whitespace for URL issue.  Previously, whitespace will lead to error.

--- app/controllers/predict_controller.rb
+++ app/controllers/predict_controller.rb
@@ -4,6 +4,10 @@ class PredictController < ApplicationController
   def delegate
     # From the input string, detect whether it should be routed to user or
     # business.
+    # First, make sure we strip the query string of any leading/trailing
+    # whitespaces, before we try to process it.
+    params[:predict_params][:query_str] =
+        params[:predict_params][:query_str].strip
     type = get_search_type params[:predict_params][:query_str]
 
     if type == "user"
